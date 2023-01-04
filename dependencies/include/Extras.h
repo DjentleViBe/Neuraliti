ba2e4a2acb9335c9b962f064204697cd98c0f59b
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2023-01-04 14:14:24 +0100

    extras

--- dependencies/include/Extras.h
+++ dependencies/include/Extras.h
@@ -3,15 +3,18 @@
 
 #include <iostream>
 #include <stdio.h>
+#include "picojson.h"
 
 extern std::string logs;
 extern int verbose;
 extern std::vector<std::string> fontlist, configlist;
+extern picojson::value v;
 
 void addlogs(std::string verbose);
 std::vector<std::string> listfiles(std::string path, std::string type);
 std::vector<std::string> splitString(const std::string& input, char delimiter);
 std::vector<std::string> readfile(char const *filename);
 std::string readfileconcat(char const *filename);
+std::string GetCurrentWorkingDirectory();
 
 #endif /* Extras_h */
