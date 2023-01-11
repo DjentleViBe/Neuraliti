c778289a3490697d159b1ae701928622cd5f3a9d
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2023-01-11 19:02:59 +0100

    window pop up size

--- dependencies/include/uielements.h
+++ dependencies/include/uielements.h
@@ -1,6 +1,6 @@
 #ifndef UIELEMENTS_H    // To make sure you don't declare the function more than once by including the header multiple times.
 #define UIELEMENTS_H
-
+extern int window_width, window_height;
 int INITgraphics();
 void Displayloop(char **argv);
 
