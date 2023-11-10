ee6985546ab9305865f9aec0fa3e812938cf79bc
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2023-11-10 09:54:11 +0100

    math operations

---
+++ dependencies/backends/MathOperations.hpp
@@ -0,0 +1,6 @@
+#ifndef MathOperations_hpp
+#define MathOperations_hpp
+
+float* concatenate(float arr1[], float arr2[], int m, int n,  size_t arra2);
+unsigned int* concatenate(unsigned int arr1[], unsigned int arr2[], int m, int n,  size_t arra2);
+#endif /* MathOperations_hpp */
