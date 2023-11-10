3ec7a9d78c87a7b1713bbafaf6688d8da5430218
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2023-11-10 09:53:41 +0100

    create window

--- dependencies/backends/CreateWindow.hpp
+++ dependencies/backends/CreateWindow.hpp
@@ -4,5 +4,5 @@
 
 #include <stdio.h>
 void DrawRectangle();
-int InitRectangle(size_t vert, float vertices[], size_t ind, unsigned int indices[], unsigned int &texture, unsigned int &VBO, unsigned int &VAO, unsigned int &EBO);
+int InitRectangle(size_t vert, float vertices[], size_t ind, unsigned int indices[], unsigned int &texture, unsigned int &VBO, unsigned int &VAO, unsigned int &EBO, const char* shadevs, const char* shadefs);
 #endif /* CreateWindow_hpp */
