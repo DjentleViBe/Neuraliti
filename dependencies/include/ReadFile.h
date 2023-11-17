b5aa0d36bd06c4723825510984c0be1d8439aec7
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2023-11-17 18:33:22 +0100

    readfile

---
+++ dependencies/include/ReadFile.h
@@ -0,0 +1,6 @@
+#ifndef ReadFile_h
+#define ReadFile_h
+
+#include <stdio.h>
+
+#endif /* ReadFile_hpp */
---
+++ src/ReadFile.cpp
@@ -0,0 +1,8 @@
+//
+//  ReadFile.cpp
+//  GENT
+//
+//  Created by Vishakh Begari on 16.04.24.
+//
+
+#include "ReadFile.hpp"
