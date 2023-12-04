31b3de2baa0e8b29e98c4cdca0133f247803d3bb
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2023-12-04 23:50:35 +0100

    windowing

---
+++ dependencies/include/Windowing.h
@@ -0,0 +1,6 @@
+#ifndef Windowing_h
+#define Windowing_h
+
+#include <stdio.h>
+
+#endif /* Windowing_hpp */
---
+++ src/Windowing.cpp
@@ -0,0 +1,8 @@
+//
+//  Windowing.cpp
+//  GENT
+//
+//  Created by Vishakh Begari on 18.04.24.
+//
+
+#include "Windowing.hpp"
