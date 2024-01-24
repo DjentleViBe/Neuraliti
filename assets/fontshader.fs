377216c6b71d60b39557cde5b07764ae68204ee8
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-01-24 02:06:35 +0100

    projection

--- assets/fontshader.fs
+++ assets/fontshader.fs
@@ -11,11 +11,8 @@ void main()
 {
     if(texture(texture1, TexCoord).r == 0.0)
     {
-        //FragColor = vec4(ourColor, 0.0);
         // If it's black, make it transparent
-        //FragColor = vec4(1, 1, 1, 1);
         FragColor = vec4(fontColor, 0.0);
-        //discard;
     }
     else{
         FragColor = vec4(fontColor, texture(texture1, TexCoord).r);
--- bin/fontshader.vs
+++ bin/fontshader.vs
@@ -10,7 +10,7 @@ out vec2 TexCoord;
 
 void main()
 {
-    gl_Position = vec4(aPos, 1.0);
+    gl_Position = ProjMat * vec4(aPos, 1.0);
     fontColor = aColor;
     TexCoord = aTexCoord;
 }
