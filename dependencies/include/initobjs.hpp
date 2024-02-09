162a1371ec12ccc90e0b67daea84db71c83cbf4c
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-02-09 18:39:25 +0100

    init objs

---
+++ dependencies/include/initobjs.hpp
@@ -0,0 +1,6 @@
+#ifndef initobjs_hpp
+#define initobjs_hpp
+
+#include <stdio.h>
+
+#endif /* initobjs_hpp */
---
+++ src/initobjs.cpp
@@ -0,0 +1,8 @@
+//
+//  initobjs.cpp
+//  GENT
+//
+//  Created by Vishakh Begari on 29.04.24.
+//
+
+#include "initobjs.hpp"
