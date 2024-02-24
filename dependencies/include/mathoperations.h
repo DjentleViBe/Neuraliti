c7fc547850a20f69224d7cd7bdaf9ae9cc8b5e08
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-02-25 00:31:37 +0100

    missing header

--- dependencies/include/mathoperations.h
+++ dependencies/include/mathoperations.h
@@ -1,6 +1,7 @@
 #ifndef mathoperations_h
 #define mathoperations_h
 
+#include <cstddef>
 float* concatenate(float arr1[], float arr2[], int m, int n,  size_t arra2);
 unsigned int* concatenate(unsigned int arr1[], unsigned int arr2[], int m, int n,  size_t arra2);
 #endif /* mathoperations_h */
