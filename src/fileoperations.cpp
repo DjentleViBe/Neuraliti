8464715e123ad8926c88b6c4edbf897b4527c676
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-02-25 00:31:55 +0100

    connection matrix

--- src/fileoperations.cpp
+++ src/fileoperations.cpp
@@ -44,11 +44,56 @@ int readpdfile(std::string path){
                 wordpos++;
                 }
             objectnames.push_back(tokentemp.erase(tokentemp.size() - 1));
-            }
-        // check if connect
-        else if(lines[l].find("connect")!= std::string::npos){
-            
-            }
+            }   
         }
-    return 0;
+
+    // create connection matrix;
+    std::vector<std::vector<int>> connectionmatrix(objnumber, std::vector<int>(objnumber));
+    std::vector<int> objinlets(objnumber);
+    std::vector<int> objoutlets(objnumber);
+    for (int l = 0; l < lines.size(); l++){
+        // check if obj
+    // check if connect
+        if(lines[l].find("connect")!= std::string::npos){
+            int connectpos = 0;
+            std::stringstream cs(lines[l]);
+            std::string tokenconnect;
+            std::string tokenconnecttemp = "";
+            int obj1 = 0;
+            int obj2 = 0;
+            while (std::getline(cs, tokenconnect, delimiter)) {
+                if(connectpos == 2){
+                    // object
+                    // std::cout << "\nobject number :" << tokenconnect << "\n";
+                    obj1 = stoi(tokenconnect);
+                }
+                else if(connectpos == 3){
+                    // outlet number
+                    // std::cout << "outlet number :" << tokenconnect << "\n";
+                    objoutlets[obj1] += 1;
+                }
+                else if(connectpos == 4){
+                    // object
+                    // std::cout << "object number :" << tokenconnect << "\n";
+                    obj2 = stoi(tokenconnect);
+                    
+                }
+                else if(connectpos == 5){
+                    // inlet number
+                    // std::cout << "inlet number : " << tokenconnect << "\n";
+                    objinlets[obj2] += 1;
+                }
+                connectpos++;
+                }
+            connectionmatrix[obj1][obj2] += 1;
+            std::cout << "\n";
+        }
     }
+    printMatrix(connectionmatrix, "Connection matrix");
+    printvector(objinlets, "Inlets");
+    printvector(objoutlets, "Outlets");
+
+    return 0;
+}
+
+
