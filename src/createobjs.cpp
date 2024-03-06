f2b36736b2d7afa3463d603d6283bb75370cb623
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-03-06 01:45:36 +0100

    clean up

--- src/createobjs.cpp
+++ src/createobjs.cpp
@@ -5,26 +5,24 @@
 #include "../dependencies/include/creategeom.h"
 #include "../dependencies/include/uielements.h"
 
-float primary_color_4[]     = {0.12941, 0.12941, 0.15294};
-float primary_color_5[]     = {1.0, 1.0, 1.0};
 int createobjs(){
     return 0;
 }
 
-NeuralObj createrect(int ind, NeuralObj MyObj, float x, float y, float objtype, float color[], std::string objname){
+NeuralObj createrect(int ind, NeuralObj MyObj, float x, float y, float objtype, float* color, std::string objname){
     MyObj.x = x;
     MyObj.y = y;
     MyObj.objtype = objtype;
     MyObj.color = color;
     MyObj.objname = objname;
     MyObj.Inletnum = globalinlets[ind];
+    MyObj.Outletnum = globaloutlets[ind];
     MyObj = createobj(MyObj);
-    // loop through inlets 
-    
+
     return MyObj;
 }
 
-NeuralObj createfont(NeuralObj MyObj, float x, float y, float objtype, float color[], std::string objname){
+NeuralObj createfont(NeuralObj MyObj, float x, float y, float objtype, float* color, std::string objname){
     MyObj.x = x;
     MyObj.y = y;
     MyObj.objtype = objtype;
@@ -38,10 +36,10 @@ NeuralObj createfont(NeuralObj MyObj, float x, float y, float objtype, float col
 NeuralObj createobj1(int ind, float x, float y, std::string objname, int neuralobjtype){
     NeuralObj MyObj3;
     if(neuralobjtype == 0){
-        MyObj3 = createrect(ind, MyObj3, x, y, 0, primary_color_4, objname);
+        MyObj3 = createrect(ind, MyObj3, x, y, 0, primary_color_2, objname);
     }
     else{
-        MyObj3 = createfont(MyObj3, x, y, 0, primary_color_5, objname);
+        MyObj3 = createfont(MyObj3, x, y, 0, primary_color_3, objname);
     }
     // create inlets and outlets
 
