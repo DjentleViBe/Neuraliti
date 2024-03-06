dd6b8278d7229536ed1f120f6798dedc9c41f555
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-03-06 01:45:09 +0100

    imgui theme

--- dependencies/backends/imgui_draw.cpp
+++ dependencies/backends/imgui_draw.cpp
@@ -179,7 +179,7 @@ void ImGui::StyleColorsLight(ImGuiStyle* dst)
 
     colors[ImGuiCol_Text]                   = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
     colors[ImGuiCol_TextDisabled]           = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
-    colors[ImGuiCol_WindowBg]               = ImVec4(0.1294f, 0.1294f, 0.1490f, 1.00f);
+    colors[ImGuiCol_WindowBg]               = ImVec4(0.1294f, 0.1568f, 0.1960f, 1.00f);
     colors[ImGuiCol_ChildBg]                = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
     colors[ImGuiCol_PopupBg]                = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
     colors[ImGuiCol_Border]                 = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
