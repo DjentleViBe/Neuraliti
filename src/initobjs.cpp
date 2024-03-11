dcbb72e56384f4304582d8fd4bbc84fa8d38f130
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-03-11 04:06:23 +0100

    setup connections

--- src/initobjs.cpp
+++ src/initobjs.cpp
@@ -5,11 +5,53 @@
 #include <vector>
 #include "../dependencies/include/uielements.h"
 #include "../dependencies/include/extras.h"
+#include "../dependencies/include/datatypes.hpp"
+#include "../dependencies/include/createobjs.hpp"
+#include <sstream>
 
 int initobjs(std::string path){
     auto result = readpdfile(path);
     globalinlets = std::get<0>(result);
     globaloutlets = std::get<1>(result);
     return 0;
+}
+
+NeuralLines* setupconnections(NeuralObj* MyObj_rect, std::string path){
+    std::cout << connectnumber << "\n";
+    NeuralLines *MyObj_line = new NeuralLines[connectnumber];
+    char delimiter = ' ';
+    std::vector<std::string> lines;
+    lines = readfile(path.c_str());
+    std::string token;
+    int linenumber = 0;
+    for (int l = 0; l < lines.size(); l++){
+        std::stringstream ss(lines[l]);
+        int fromobj, fromoutlet, toobj, toinlet;
+        if(lines[l].find("connect")!= std::string::npos){
+            int wordpos = 0;
+            while (std::getline(ss, token, delimiter)){
+                if(wordpos == 2){
+                    // object from number
+                    fromobj = stoi(token);
+                }
+                else if(wordpos == 3){
+                    // outlet number
+                    fromoutlet = stoi(token);
+                }
+                else if(wordpos == 4){
+                    // object to number
+                    toobj = stoi(token);
+                }
+                else if(wordpos == 5){
+                    toinlet = stoi(token);
+                }
+                wordpos++;
+            }
+            // determine x y from objnumber
+            MyObj_line[linenumber] = createline1(MyObj_rect[fromobj].x, MyObj_rect[fromobj].y, MyObj_rect[toobj].x, MyObj_rect[toobj].y);
+            linenumber++;
+        }
+    }
+    return MyObj_line;
 }
 
