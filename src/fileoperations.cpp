35df3f7a49b722942086e6bfe9c5d3aab1eb6f13
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-03-22 21:56:00 +0100

    file operatioons

--- src/fileoperations.cpp
+++ src/fileoperations.cpp
@@ -13,8 +13,12 @@ int connectnumber = 0;
 std::vector<float> Xposition;
 std::vector<float> Yposition;
 std::vector<std::string> objectnames;
+// create connection matrix;
+std::vector<int> objinlets;
+std::vector<int> objoutlets;
 
 std::tuple<std::vector<int>, std::vector<int>> readpdfile(std::string path){
+    std::cout << "reading file\n" << std::endl;
     std::vector<std::string> lines;
     char delimiter = ' ';
     lines = readfile(path.c_str());
@@ -23,7 +27,7 @@ std::tuple<std::vector<int>, std::vector<int>> readpdfile(std::string path){
     for (int l = 0; l < lines.size(); l++){
         // check if obj
         if(lines[l].find("obj")!= std::string::npos){
-            objnumber++;
+            
             int wordpos = 0;
             std::stringstream ss(lines[l]);
             std::string token;
@@ -31,29 +35,33 @@ std::tuple<std::vector<int>, std::vector<int>> readpdfile(std::string path){
             while (std::getline(ss, token, delimiter)) {
                 tokens.push_back(token);
                 if(wordpos == 2){
+                    // x position
                     Xposition.push_back(stof(token));
                 }
                 else if(wordpos == 3){
+                    // y position
                     Yposition.push_back(stof(token));
                 }
                 else if(wordpos == 4){
+                    // object name
+                    objinlets.push_back(1);
+                    objoutlets.push_back(1);
                     tokentemp += token;
                 }
                 else if(wordpos > 4){
+                    objinlets[objnumber] += 1;
                     tokentemp += " " + token;
                 }
                 wordpos++;
                 }
             objectnames.push_back(tokentemp.erase(tokentemp.size() - 1));
+            objnumber++;
             }   
         }
 
-    // create connection matrix;
     std::vector<std::vector<int>> connectionmatrix(objnumber, std::vector<int>(objnumber));
-    std::vector<int> objinlets(objnumber);
-    std::vector<int> objoutlets(objnumber);
     for (int l = 0; l < lines.size(); l++){
-        // check if obj
+    // check if obj
     // check if connect
         if(lines[l].find("connect")!= std::string::npos){
             connectnumber++;
@@ -67,34 +75,34 @@ std::tuple<std::vector<int>, std::vector<int>> readpdfile(std::string path){
                 if(connectpos == 2){
                     // object
                     // std::cout << "\nobject number :" << tokenconnect << "\n";
-                    obj1 = stoi(tokenconnect);
+                    // obj1 = stoi(tokenconnect);
                 }
                 else if(connectpos == 3){
                     // outlet number
                     // std::cout << "outlet number :" << tokenconnect << "\n";
-                    objoutlets[obj1] += 1;
+                    // objoutlets[obj1] += 1;
                 }
                 else if(connectpos == 4){
                     // object
                     // std::cout << "object number :" << tokenconnect << "\n";
-                    obj2 = stoi(tokenconnect);
+                    // obj2 = stoi(tokenconnect);
                     
                 }
                 else if(connectpos == 5){
                     // inlet number
                     // std::cout << "inlet number : " << tokenconnect << "\n";
-                    objinlets[obj2] += 1;
+                    // objinlets[obj2] += 1;
                 }
                 connectpos++;
                 }
-            connectionmatrix[obj1][obj2] += 1;
+            // connectionmatrix[obj1][obj2] += 1;
             std::cout << "\n";
         }
     }
-    printMatrix(connectionmatrix, "Connection matrix");
+    // printMatrix(connectionmatrix, "Connection matrix");
     //printvector(objinlets, "Inlets");
     //printvector(objoutlets, "Outlets");
-
+    //std::cout << "read\n";
     return std::make_tuple(objinlets, objoutlets);
 }
 
