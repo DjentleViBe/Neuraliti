3c56b4acca927f906994b4f294459b0956d53575
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-05-28 05:49:05 +0200

    subtraction library

---
+++ sharedlib/subtract.cpp
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <iostream>
+#include <cstdarg>
+
+extern "C" {
+    float sub_(int count, ...) {
+        va_list args;
+        va_start(args, count);
+        float sum = 0;
+        for (int i = 0; i < count; ++i) {
+            sum -= va_arg(args, double);
+        }
+        va_end(args);
+        std::cout << sum << std::endl;
+        return sum;
+    }
+}
