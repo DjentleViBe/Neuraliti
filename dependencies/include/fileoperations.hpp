52fc67fab5073d9e953789c2e0b34d0f67567565
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-05-31 08:36:13 +0200

    connection matrix global

--- dependencies/include/fileoperations.hpp
+++ dependencies/include/fileoperations.hpp
@@ -10,6 +10,6 @@ extern std::vector<std::string> objectnames;
 extern std::vector<int> objecttypes;
 extern int objnumber;
 extern int connectnumber;
-
+extern std::vector<std::vector<int>> connectionmatrix;
 std::tuple<std::vector<int>, std::vector<int>> readpdfile(std::string path);
 #endif /* fileoperations_hpp */
