8f20be9aa69c83467b62346c1828fd13d603f463
DjentleViBe <116117854+DjentleViBe@users.noreply.github.com>
2024-06-08 18:57:19 +0200

    headers

--- dependencies/include/menu.h
+++ dependencies/include/menu.h
@@ -7,6 +7,7 @@
 extern bool saveas;
 extern bool openfile;
 extern bool editpref;
+extern bool audiopref;
 extern std::string filename;
 extern std::map<std::string, std::string> appsettings;
 
--- dependencies/include/pa.hpp
+++ dependencies/include/pa.hpp
@@ -1,6 +1,9 @@
 #ifndef PA_HPP
 #define PA_HPP
 #include "portaudio.h"
+#include <vector>
+
+extern std::vector<std::string> audioinputdevicelist, audiooutputdevicelist;
 
 int getnumaudiodevices();
 const PaDeviceInfo* getaudiodeviceinfo(int i);
